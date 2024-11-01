f1e4e93c98574552c05ae65520ba017325a19d54
yogeswara badepalli <153403816+yogibadepalli@users.noreply.github.com>
2024-11-01 00:06:12 +0000

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -9,7 +9,9 @@ def load_data(file_path):
 def preprocess_data(df):
     """Cleans and preprocesses the dataset."""
     # Drop low-quality sensor readings and irrelevant columns
-    columns_to_drop = ['hand_acceleration(±6g)_x', 'hand_orientation_1', 'chest_orientation_4', ...]  # Fill in the rest based on your file
+    columns_to_drop= ['hand_acceleration(±6g)_x','hand_acceleration(±6g)_y',  'hand_acceleration(±6g)_z','hand_orientation_1',  'hand_orientation_2','hand_orientation_3',  'hand_orientation_4',
+                   'chest_acceleration(±6g)_x','chest_acceleration(±6g)_y',  'chest_acceleration(±6g)_z','chest_orientation_1',  'chest_orientation_2','chest_orientation_3',  'chest_orientation_4',
+                    'ankle_acceleration(±6g)_x','ankle_acceleration(±6g)_y',  'ankle_acceleration(±6g)_z','ankle_orientation_1',  'ankle_orientation_2', 'ankle_orientation_3',  'ankle_orientation_4']  # Fill in the rest based on your file
     df.drop(columns=columns_to_drop, inplace=True)
     
     # Fill missing heart rate data using forward-fill
